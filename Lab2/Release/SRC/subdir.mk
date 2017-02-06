5e39c1a44810fa5ec9c0ee1b4aa98f9d5fa9fea3
nbeeten <nbeeten@AK120D-04.admin.wpi.edu>
2017-02-05 23:14:25 -0500

    These might be important and they aren't being blocked by my .gitignore so I am going to include them. (I think that this means that they were added before and I am not sure how to remove them from being tracked by git)

--- Lab2/Release/SRC/subdir.mk
+++ Lab2/Release/SRC/subdir.mk
@@ -5,28 +5,46 @@
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
 ../SRC/ADC.c \
+../SRC/ADCcalc.c \
+../SRC/DAC.c \
+../SRC/PID.c \
+../SRC/SPI.c \
 ../SRC/USART.c \
+../SRC/currentSense.c \
 ../SRC/freerunADC.c \
 ../SRC/globals.c \
 ../SRC/main.c \
+../SRC/motors.c \
 ../SRC/ports.c \
 ../SRC/timer.c 
 
 OBJS += \
 ./SRC/ADC.o \
+./SRC/ADCcalc.o \
+./SRC/DAC.o \
+./SRC/PID.o \
+./SRC/SPI.o \
 ./SRC/USART.o \
+./SRC/currentSense.o \
 ./SRC/freerunADC.o \
 ./SRC/globals.o \
 ./SRC/main.o \
+./SRC/motors.o \
 ./SRC/ports.o \
 ./SRC/timer.o 
 
 C_DEPS += \
 ./SRC/ADC.d \
+./SRC/ADCcalc.d \
+./SRC/DAC.d \
+./SRC/PID.d \
+./SRC/SPI.d \
 ./SRC/USART.d \
+./SRC/currentSense.d \
 ./SRC/freerunADC.d \
 ./SRC/globals.d \
 ./SRC/main.d \
+./SRC/motors.d \
 ./SRC/ports.d \
 ./SRC/timer.d 
 
@@ -35,7 +53,7 @@ C_DEPS += \
 SRC/%.o: ../SRC/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: AVR Compiler'
-	avr-gcc -I"C:\Users\Jonathan Tai\git\RBELib2\RBELib\include" -I"C:\WinAVR-20100110\avr\include" -I"C:\WinAVR-20100110\lib\gcc\avr\4.3.3\include" -I"C:\WinAVR-20100110\lib\gcc\avr\4.3.3\include-fixed" -Wall -Os -fpack-struct -fshort-enums -ffunction-sections -fdata-sections -std=gnu99 -funsigned-char -funsigned-bitfields -mmcu=atmega644p -DF_CPU=18432000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	avr-gcc -I"C:\Users\nbeeten\git\rbelib\RBELib\include" -I"C:\WinAVR-20100110\avr\include" -I"C:\WinAVR-20100110\lib\gcc\avr\4.3.3\include" -I"C:\WinAVR-20100110\lib\gcc\avr\4.3.3\include-fixed" -Wall -Os -fpack-struct -fshort-enums -ffunction-sections -fdata-sections -std=gnu99 -funsigned-char -funsigned-bitfields -mmcu=atmega644p -DF_CPU=18432000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
